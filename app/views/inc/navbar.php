3dcb07e3955067fd45efd369b00460c4c0dabfdb
JonathanJair25 <jonathan_vive@hotmail.com>
2024-08-04 22:43:38 -0600

    ajustes de la caja de busqueda

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,18 +1,20 @@
 <div class="full-width navBar">
     <div class="full-width navBar-options">
-        <i class="fas fa-exchange-alt fa-fw" id="btn-menu"></i>
         <nav class="navBar-options-list">
             <ul class="list-unstyle">
-                <li class="noLink">
+                            <li class="noLink">
                     <div class="input-group">
-                        <span class="input-group-addon"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></span>
+                        <span class="input-group-addon">
+                            <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+                        </span>
                         <input type="text" class="form-control" id="search" placeholder="Buscar cliente..." style="width: 500px; padding: 7px">
                     </div>
-                    <div id="result-container" style="position: left; top: 100%; left: 0; width: 500px; z-index: 1000;">
-                        <div id="result" style="border: 1px solid #ccc; background-color: #000; max-height: 300px; overflow-y: auto;">
+                    <div id="result-container" style="position: absolute; top: 100%; left: 0; width: 500px; z-index: 1000;">
+                        <div id="result" style="border: 1px solid #ccc; background-color: #a0a0a0; color: #000000; max-height: 300px; overflow-y: auto;">
                         </div>
                     </div>
                 </li>
+                <i class="fas fa-exchange-alt fa-fw" id="btn-menu"></i>
                 <li class="text-condensedLight noLink">
                     <a class="btn-exit" href="<?php echo APP_URL."logOut/"; ?>">
                         <i class="fas fa-power-off"></i>
@@ -25,11 +27,12 @@
                     <?php
                         if(is_file("./app/views/fotos/".$_SESSION['foto'])){
                             echo '<img class="is-rounded img-responsive" src="'.APP_URL.'app/views/fotos/'.$_SESSION['foto'].'">';
-                        }else{
+                        } else {
                             echo '<img class="is-rounded img-responsive" src="'.APP_URL.'app/views/fotos/default.png">';
                         }
                     ?>
                 </li>
+                
             </ul>
         </nav>
     </div>
